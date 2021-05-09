97c36d7ade5e1b39331eba5d74463ab9f441a665
Mounika7777 <74643028+Mounika7777@users.noreply.github.com>
2021-05-09 20:12:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var database;
 var height;
 
 function preload(){
-   bg =loadImage("Images/cityImage.png");
+   bg =loadImage("cityImage.png");
    balloonImage1=loadAnimation("HotAirBallon-01.png");
    balloonImage2=loadAnimation("HotAirBallon-01.png","HotAirBallon-01.png",
    "HotAirBallon-01.png","HotAirBallon-02.png","HotAirBallon-02.png",
